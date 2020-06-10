fa2f2508a2da5700badaffcfe056a4f1ace90174
moh fajar <mohfajar1967@gmail.com>
2020-06-10 13:44:47 +0700

    image profile (2)

---
+++ app/src/main/java/com/example/aismobile/Personalia/PersonaliaMenuActivity.java
@@ -0,0 +1,16 @@
+package com.example.aismobile.Personalia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aismobile.R;
+
+public class PersonaliaMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personalia_menu);
+    }
+}
